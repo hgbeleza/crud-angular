08800d9c2482d761d35cc0ee684f7d2fce183009
hgbeleza <developer.beleza06@gmail.com>
2024-11-12 21:54:26 -0700

    feat: create interface demo

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/Course';
 
 @Component({
   selector: 'app-courses',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
-
+  courses: Course[] = [
+    { id: 1, name: 'Angular + Spring', category: 'fullstack' },
+    { id: 2, name: 'Java Spring Boot', category: 'Backend' },
+    { id: 3, name: 'Angular 17', category: 'Frontend' },
+    { id: 4, name: 'angular + spring', category: 'fullstack' },
+    { id: 5, name: 'angular + spring', category: 'fullstack' },
+    { id: 6, name: 'angular + spring', category: 'fullstack' },
+    { id: 7, name: 'angular + spring', category: 'fullstack' },
+    { id: 8, name: 'angular + spring', category: 'fullstack' }
+  ];
 }
